9a98303960770aafb2d71b46beec53bd211a70e0
bahoang3105 <73835461+bahoang3105@users.noreply.github.com>
2021-10-20 16:20:00 +0700

    Update webpack.config.js

--- canvas_react_tutorial/webpack.config.js
+++ canvas_react_tutorial/webpack.config.js
@@ -2,57 +2,51 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'build'),
-    },
-    devServer: {
-        static: path.join(__dirname, 'build'),
-        compress: true,
-        port: 3005,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                exclude: /node_module/,
-                use: ['style-loader', 'css-loader'],
-            },
+	entry: './src/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'build'),
+	},
+	devServer: {
+		static: path.join(__dirname, 'build'),
+		compress: true,
+		port: 3005,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				exclude: /node_module/,
+				use: ['style-loader', 'css-loader'],
+			},
 
-            {
-                test: /\.js$/,
-                exclude: /node_module/,
-                loader: 'babel-loader',
-                options: {
-                    presets: [
-                        ['@babel/preset-env', { targets: { esmodules: true } }],
-                        ['@babel/preset-react', { targets: { esmodules: true }, runtime: "automatic" }],
-                        // [
-                        //     require.resolve('babel-preset-react-app'),
-                        //     {
-                        //       runtime: 'automatic',
-                        //     },
-                        // ],
-                    ]
-                },
-            },
+			{
+				test: /\.js$/,
+				exclude: /node_module/,
+				loader: 'babel-loader',
+				options: {
+					presets: [
+						['@babel/preset-env', { targets: { esmodules: true } }],
+						['@babel/preset-react', { targets: { esmodules: true }, runtime: "automatic" }],
+					]
+				},
+			},
 
-						{
-							test: /\.(png|jpe?g|gif)$/i,
-							exclude: /node_module/,
-							loader: 'file-loader'
-						}
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({ 
-            filename: 'index.html',
-            template: './public/index.html',
-        }),
-    ],
-    mode: 'development',
-    externals: {
-        'React': 'react',
-    },
+			{
+				test: /\.(png|jpe?g|gif)$/i,
+				exclude: /node_module/,
+				loader: 'file-loader'
+			}
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({ 
+			filename: 'index.html',
+			template: './public/index.html',
+		}),
+	],
+	mode: 'development',
+	externals: {
+		'React': 'react',
+	},
 }
